6d4342f5316c143b31bf2bad86ab9b6e75dce55b
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2026-01-24 23:19:16 +0300

    a

--- frontend/src/pages/backoffice/dashboard0/index.tsx
+++ frontend/src/pages/backoffice/dashboard0/index.tsx
@@ -1,6 +1,6 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
-import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
+import { ChartAreaInteractive } from "@/components/shadcn/chart-area-interactive";
+import { DataTable } from "@/components/shadcn/data-table";
+import { SectionCards } from "@/components/shadcn/section-cards";
 import data from "./data.json";
 
 export default function Page() {
