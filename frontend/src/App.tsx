27b926c5c0837c9a73abfe8c9a75a88de8e1d04e
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2026-01-25 21:39:15 +0300

    init sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,24 @@ import { AuthProvider } from "./context/AuthContext";
 import { useI18n } from "@/hooks/useI18n";
 import { ThemeProvider } from "./context/ThemeContext";
 import { HeaderProvider } from "./context/HeaderContext";
+import { Toaster } from "@/components/ui/sonner";
 
 const App = () => {
   useI18n();
-  
+
   return (
-    <ThemeProvider>
-      <HeaderProvider>
-        <AuthProvider>
-          <BrowserRouter>
-            <AppRoute />
-          </BrowserRouter>
-        </AuthProvider>
-      </HeaderProvider>
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider>
+        <HeaderProvider>
+          <AuthProvider>
+            <BrowserRouter>
+              <AppRoute />
+            </BrowserRouter>
+          </AuthProvider>
+        </HeaderProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
--- frontend/src/pages/backoffice/sync/Index.tsx
+++ frontend/src/pages/backoffice/sync/Index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useHeader } from "@/context/HeaderContext";
 import { useTranslation } from "react-i18next";
+import SonnerTypes from "@/components/test/SonnerType";
 
 const Index = () => {
   const { t } = useTranslation();
@@ -15,6 +16,7 @@ const Index = () => {
   return (
     <div className="py-4 px-2 md:gap-6 md:py-6 md:px-4 max-w-full overflow-hidden">
       {t("common.sync")}
+      <SonnerTypes></SonnerTypes>
     </div>
   );
 };
